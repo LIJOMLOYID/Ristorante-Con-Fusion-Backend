afaac21f87b706cccd504f60862e06d10a94e4d3
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-25 15:50:44 +0530

    Added server

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end(
-    <html>
-      <body>
-        <h1>This is an Express Server</h1>
-      </body>
-    </html>
-  );
+  res.end("<html><body><h1>This is an Express Server<h1></body></html>");
+});
+
+const server = http.createServer(app);
+
+app.listen(port, hostname, () => {
+  console.log(`Server running http://${hostname} : ${port}`);
 });
